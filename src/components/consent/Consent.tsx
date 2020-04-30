449c4bf1d25cd8497322b1642fa39e5e6804fbd5
Alina Gendel <alina.gendel@sagebase.org>
2020-04-30 12:06:31 -0700

    consent and other stuff

--- src/components/consent/Consent.tsx
+++ src/components/consent/Consent.tsx
@@ -20,6 +20,8 @@ import MarkdownSynapse from 'synapse-react-client/dist/containers/MarkdownSynaps
 import Button from '@material-ui/core/Button/Button'
 import TextField from '@material-ui/core/TextField/TextField'
 import { Checkbox, FormControlLabel } from '@material-ui/core'
+import Alert from '@material-ui/lab/Alert/Alert'
+import ConsentEHR from './ConsentEHR'
 
 export type ConsentProps = {
   token: string
@@ -35,11 +37,13 @@ export const Consent: React.FunctionComponent<ConsentProps> = ({
   const [isInfoDone, setIsInfoDone] = useState(false)
   const [isConsentDone, setIsConsentDone] = useState(false)
   const [isLearnMore, setIsLearnMore] = useState(false)
+  const [error, setError]= useState('')
 
   const stateSchema = {
     agree: { value: '', error: '' },
     shareAll: { value: '', error: '' },
     fullName: { value: '', error: '' },
+    dob: { value: '', error: '' },
   }
 
   const validationStateSchema = {
@@ -62,19 +66,24 @@ export const Consent: React.FunctionComponent<ConsentProps> = ({
       scope: state.shareAll ? SHARE_SCOPE_ALL : SHARE_SCOPE_PARTNERS,
       signedOn: moment().toLocaleString(),
     }
-    console.log('about to call end point')
+  try{
+    setError('')
     const result = await callEndpoint(
       `${ENDPOINT}/v3/subpopulations/${SUBPOP_GUID}/consents/signature`,
       'POST',
       data,
       token
     )
-    // alert(JSON.stringify(result, null, 2))
+
     setIsConsentDone(true)
     if (setConsentFn) {
       //setConsentFn(result.ok)
+    }}
+    catch(e) {
+
+      setError(e.error.message)
     }
-    // return result
+  
   }
 
   const { state, handleOnChange, handleOnSubmit, disable } = useForm(
@@ -89,12 +98,14 @@ export const Consent: React.FunctionComponent<ConsentProps> = ({
     })
   }
 
-  if (isConsentDone) {
+
+  /*if (isConsentDone) {
     return <Redirect to="Dashboard"></Redirect>
-  }
+  }*/
 
   return (
     <div className="Consent">
+     
       {!isInfoDone && (
         <div className="Consent__info">
           <ConsentInfo
@@ -113,72 +124,102 @@ export const Consent: React.FunctionComponent<ConsentProps> = ({
           <MarkdownSynapse ownerId="syn21985841" wikiId="602371" />
           Please check the box below if you agree to take part:
           <form className="Consent__form" onSubmit={handleOnSubmit}>
-            <div className="form-group">
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    color="primary"
-                    value={state.shareAll.value}
-                    onChange={(val) => checkboxChange('shareAll')}
-                  />
-                }
-                label="Share my data with this study as well as other qualified researchers <strong>for future research </strong> on COVID related work "
+            <div
+              className="form-group"
+              style={{
+                display: 'flex',
+                alignItems: 'flex-start',
+                paddingTop: '16px',
+              }}
+            >
+              <Checkbox
+                color="primary"
+                style={{ paddingTop: '3px' }}
+                value={state.shareAll.value}
+                onChange={(val) => checkboxChange('shareAll')}
               />
-              <div
-                style={{
-                  display: isLearnMore ? 'block' : 'none',
-                  borderBottom: '1px solid #ddd',
-                }}
-              >
-                some text about learning more about reserch sharing
-                <Button
-                  style={{ float: 'right' }}
-                  onClick={() => setIsLearnMore(false)}
-                >
-                  <FontAwesomeIcon icon={faCaretUp}></FontAwesomeIcon>
-                </Button>
-              </div>
-              <div
-                style={{
-                  display: isLearnMore ? 'none' : 'block',
-                  borderBottom: '1px solid #ddd',
-                }}
-              >
-                Learn More
-                <Button
-                  style={{ float: 'right' }}
-                  onClick={() => setIsLearnMore(true)}
-                >
-                  <FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon>
-                </Button>
-              </div>
+
+              <p>
+                "Share my data with this study as well as other qualified
+                researchers <strong>for future research </strong> on COVID
+                related work "
+              </p>
             </div>
-            <div className="form-group">
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    color="primary"
-                    value={state.agree.value}
-                    onChange={(val) => checkboxChange('agree')}
-                  />
-                }
-                label="I have read this consent form (or someone read it to me). I understand the information in this form. All of my questions have been answered. I freely and willingly choose to take part in NY Strong."
+
+            <div
+              style={{
+                display: isLearnMore ? 'flex' : 'none',
+                borderBottom: '1px solid #ddd',
+
+                justifyContent: 'space-between',
+                alignItems: 'flex-end',
+              }}
+            >
+              <p>
+                some text about learning
+                <br /> more about <br />
+                reserch sharing
+              </p>
+              <Button
+                style={{ float: 'right' }}
+                onClick={() => setIsLearnMore(false)}
+              >
+                <FontAwesomeIcon icon={faCaretUp}></FontAwesomeIcon>
+              </Button>
+            </div>
+            <div
+              style={{
+                display: isLearnMore ? 'none' : 'block',
+                borderBottom: '1px solid #ddd',
+              }}
+            >
+              Learn More
+              <Button
+                style={{ float: 'right' }}
+                onClick={() => setIsLearnMore(true)}
+              >
+                <FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon>
+              </Button>
+            </div>
+
+            <div
+              className="form-group"
+              style={{
+                display: 'flex',
+                alignItems: 'flex-start',
+                paddingTop: '16px',
+                marginLeft: '-40px',
+              }}
+            >
+              <Checkbox
+                color="primary"
+                style={{ paddingTop: '3px' }}
+                value={state.agree.value}
+                onChange={(val) => checkboxChange('agree')}
               />
+              <p>
+                I have <b>read</b> this consent form (or someone read it to me).
+                I understand the information in this form. All of my questions
+                have been answered. I <strong>freely and willingly</strong>{' '}
+                choose to take part in NY Strong."
+              </p>
             </div>
             <p>{moment().format('MMMM Do, YYYY')}</p>
-            <TextField
-              label="Full Name of adult participant:"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              fullWidth
-              onChange={handleOnChange}
-              value={state.fullName.value}
-              name="fullName"
-              variant="outlined"
-            />
-
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div className="form-group">
+              <TextField
+                label="Full Name of adult participant:"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                fullWidth
+                onChange={handleOnChange}
+                value={state.fullName.value}
+                name="fullName"
+                variant="outlined"
+              />
+            </div>
+            {error && <Alert severity="error">{error}</Alert>}
+            <div style={{ display: 'flex', justifyContent: 'space-between' , paddingTop: "20px"}}>
               <Button
                 onClick={() => alert('todo')}
                 variant="contained"
@@ -217,6 +258,7 @@ export const Consent: React.FunctionComponent<ConsentProps> = ({
           </Button>
         </>
       )}
+      {isConsentDone && <ConsentEHR></ConsentEHR>}
     </div>
   )
 }
