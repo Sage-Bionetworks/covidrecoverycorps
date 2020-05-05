e6052dcfb8efe5635b377a042bb02ba680065122
jay-hodgson <jay.hodgson@sagebionetworks.org>
2020-05-04 21:07:54 -0700

    with a functional mailchimp signup

--- src/components/registration/Ineligible.tsx
+++ src/components/registration/Ineligible.tsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react'
 
 
-import { IneligibilityReason } from '../../types/types'
+import { IneligibilityReason, MailChimpFormFields } from '../../types/types'
 import ToggleButton from '@material-ui/lab/ToggleButton/ToggleButton'
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup/ToggleButtonGroup'
 
 import TextField from '@material-ui/core/TextField/TextField'
 import { Button } from '@material-ui/core'
+import MailchimpSubscribe from 'react-mailchimp-subscribe'
 
 type IneligibleProps = {
   reason: IneligibilityReason
 }
+
+const url = 'https://sagebionetworks.us7.list-manage.com/subscribe/post?u=b146de537186191a9d2110f3a&amp;id=46d182222e'
 
 export const Ineligible: React.FunctionComponent<IneligibleProps> = ({
   reason,
@@ -19,6 +22,11 @@ export const Ineligible: React.FunctionComponent<IneligibleProps> = ({
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [zipcode, setZipcode] = useState('')
+  const formData:MailChimpFormFields = {
+    EMAIL: email,
+    NAME: name,
+    ZIP: zipcode
+  }
   return (
     <>
       {reason === 'AGE' && (
@@ -51,44 +59,67 @@ export const Ineligible: React.FunctionComponent<IneligibleProps> = ({
             </ToggleButtonGroup>
             {mailList && (
               <div>
-                <div className="form-group">
-           
-                <TextField
-                  name="name"
-                  value={name}
-                  fullWidth
-           
-                  placeholder="Full Name"
-                  aria-label="Full Name"
-                  variant="outlined"
-                  onChange={(e) => setName(e.target.value)}
-                  label="Full Name"
+                <MailchimpSubscribe
+                  url={url}
+                  render={({ subscribe, status, message }) => (
+                    <div>
+                      <div className="form-group">
+                        <div className="input--min-padded">
+                          <TextField
+                            name="name"
+                            value={name}
+                            fullWidth
+                            placeholder="full name"
+                            aria-label="full name"
+                            variant="outlined"
+                            onChange={(e) => setName(e.target.value)}
+                            label="Full name"
+                          />
+                        </div>
+                        <div className="input--min-padded">
+                          <TextField
+                            name="email"
+                            type="email"
+                            variant="outlined"
+                            fullWidth
+                            value={email}
+                            placeholder="email"
+                            aria-label="email"
+                            label="Email"
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                        </div>
+                        <div className="input--min-padded">
+                          <TextField
+                            name="zip"
+                            type="text"
+                            variant="outlined"
+                            fullWidth
+                            value={zipcode}
+                            placeholder="zip code"
+                            aria-label="zip code"
+                            label="Zip code"
+                            onChange={(e) => setZipcode(e.target.value)}
+                          />
+                        </div>
+                      </div>
+                      <Button variant="contained" color="primary" fullWidth onClick={
+                        () =>
+                          email &&
+                          name &&
+                          subscribe(formData)
+                      }>Join Mailing List</Button>
+                      {status === "sending" && <div>Joining Mailing List...</div>}
+                      {/* Need help on how to handle this success case Alina!
+                       {status === "success" && <div> Successfully Joined </div>} */}
+                      {status === "error" && message && (
+                        <div
+                          dangerouslySetInnerHTML={{ __html: message }}
+                        />
+                      )}
+                    </div>
+                  )}
                 />
-                <TextField
-                  name="email"
-              
-                  type="email"
-                  variant="outlined"
-                  fullWidth
-                  value={email}
-                  placeholder="email"
-                  aria-label="email"
-                  label="email address"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <TextField
-                  name="zip"
-                  type="text"
-                  variant="outlined"
-                  fullWidth
-                  value={zipcode}
-                  placeholder="zip code"
-                  aria-label="zip code"
-                  label="zip code"
-                  onChange={(e) => setZipcode(e.target.value)}
-                />
-                </div>
-                <Button variant="contained" color="primary" fullWidth onClick={()=>alert ('todo')}>Join Mailing List</Button>
               </div>
             )}
           </div>
