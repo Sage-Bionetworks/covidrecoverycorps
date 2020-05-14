9578041ec9d0b165f05782c6314ace05b98d2ddc
jay-hodgson <jay.hodgson@sagebionetworks.org>
2020-05-14 14:59:54 -0700

    another piece

--- src/components/static/Intro.tsx
+++ src/components/static/Intro.tsx
@@ -90,6 +90,20 @@ const useStyles = makeStyles(theme => ({
     fontStyle: 'italic',
     float: 'right',
     paddingTop: '3px'
+  },
+  participationHeaderDiv: {
+    backgroundColor: '#3A3A3A',
+    padding: '60px 25px 200px 25px',
+  },
+  participationHeaderText: {
+    color: '#ffffff',
+    maxWidth: '700px',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: '2rem',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    fontFamily: playfairDisplayFont,
   }
 }))
 
@@ -149,41 +163,10 @@ export const Intro: React.FunctionComponent<IntroProps> = ({
             </div>
           </Grid>
         </Grid>
-
-        <h1>Why Participate? </h1>
-        <div className="Intro__content2">
-          <p>
-            {' '}
-            There is an urgent need to come together as a community to rapidly
-            advance our understanding of COVID-19. By sharing your experience in
-            recovering from COVID-19, you can help scientists answer critical
-            questions such as: Why do some people experience very mild symptoms
-            while others get very sick? How does the body fight this virus? What
-            levels of antibodies are needed to protect us from reinfection and
-            how long might any immunity last? How does recovering from COVID-19
-            impact your health over time?{' '}
-          </p>
-          <p>
-            Your participation may also help save lives. Participants who
-            undergo serological testing and test positive for antibodies can
-            choose to donate plasma and help those still fighting for survival
-            from COVID-19.
-          </p>
-        </div>
-
-        <h1> How it works </h1>
-        <div className="Intro__content3">
-          <p>
-            {' '}
-            There is an urgent need to come together as a community to rapidly
-            advance our understanding of COVID-19. By sharing your experience in
-            recovering from COVID-19, you can help scientists answer critical
-            questions such as: Why do some people experience very mild symptoms
-            while others get very sick? How does the body fight this virus? What
-            levels of antibodies are needed to protect us from reinfection and
-            how long might any immunity last? How does recovering from COVID-19
-            impact your health over time?{' '}
-          </p>
+        <div className={classes.participationHeaderDiv}>
+          <div className={classes.participationHeaderText}>
+            By sharing your experience in recovering from COVID-19, you can help answer critical questions. 
+          </div>
         </div>
       </div>
     </div>
