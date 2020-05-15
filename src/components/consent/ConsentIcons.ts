3865f799c519e10c1a8b1d446d8f5a754b6c835b
Alina Gendel <alina.gendel@sagebase.org>
2020-05-15 11:29:47 -0700

    add ehr icons to ehr consent

--- src/components/consent/ConsentIcons.ts
+++ src/components/consent/ConsentIcons.ts
@@ -1,19 +1,52 @@
-import _01 from  '../../assets/consent_icons/_0.png'
-import _02 from  '../../assets/consent_icons/_1.png'
-import _03 from  '../../assets/consent_icons/_2.png'
-import _04 from  '../../assets/consent_icons/_3.png'
-import _05 from  '../../assets/consent_icons/_4.png'
-import _06 from  '../../assets/consent_icons/_5.png'
-import _07 from  '../../assets/consent_icons/_6.png'
-import _08 from  '../../assets/consent_icons/_7.png'
-import _09 from  '../../assets/consent_icons/_8.png'
-import _10 from  '../../assets/consent_icons/_9.png'
+import _01 from '../../assets/consent_icons/_0.png'
+import _02 from '../../assets/consent_icons/_1.png'
+import _03 from '../../assets/consent_icons/_2.png'
+import _04 from '../../assets/consent_icons/_3.png'
+import _05 from '../../assets/consent_icons/_4.png'
+import _06 from '../../assets/consent_icons/_5.png'
+import _07 from '../../assets/consent_icons/_6.png'
+import _08 from '../../assets/consent_icons/_7.png'
+import _09 from '../../assets/consent_icons/_8.png'
+import _10 from '../../assets/consent_icons/_9.png'
 
+import ehr_01 from '../../assets/ehr_icons/_0.png'
+import ehr_02 from '../../assets/ehr_icons/_1.png'
+import ehr_03 from '../../assets/ehr_icons/_2.png'
+import ehr_04 from '../../assets/ehr_icons/_3.png'
+import ehr_05 from '../../assets/ehr_icons/_4.png'
+import ehr_06 from '../../assets/ehr_icons/_5.png'
+import ehr_07 from '../../assets/ehr_icons/_6.png'
+import ehr_08 from '../../assets/ehr_icons/_7.png'
+import ehr_09 from '../../assets/ehr_icons/_8.png'
 
 
 const ConsentIcons = {
-
-consent:  [_01, _02,undefined, _03,  _04, undefined,  _05, _06, undefined, _07, _08, _09, _10]
+  consent: [
+    _01,
+    _02,
+    undefined,
+    _03,
+    _04,
+    undefined,
+    _05,
+    _06,
+    undefined,
+    _07,
+    _08,
+    _09,
+    _10,
+  ],
+  enr: [
+    ehr_01,
+    ehr_02,
+    ehr_03,
+    ehr_04,
+    ehr_05,
+    ehr_06,
+    ehr_07,
+    ehr_08,
+    ehr_09,
+  ],
 }
 
 export default ConsentIcons
