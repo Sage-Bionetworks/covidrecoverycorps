39ac7e3144a9214f9ba7f9fb9376bc4cc66e93d7
Alina Gendel <alina.gendel@sagebase.org>
2020-06-04 10:25:25 -0700

    starting i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/stable'
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom'
 //import './index.css';
 import App from './App'
+import './i18n'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root'),
 )
