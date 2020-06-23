e8e557fda4552f3cee1e5f96f7b4190060ff6232
Michael Lee <michael.leem42@gmail.com>
2020-06-23 13:07:14 -0700

    reset state var

--- src/components/consent/ConsentInfo.tsx
+++ src/components/consent/ConsentInfo.tsx
@@ -52,7 +52,7 @@ export const ConsentInfo: React.FunctionComponent<ConsentInfoProps> = ({
   onDone,
 }: ConsentInfoProps) => {
   const [isFullText, setIsFullText] = useState(false)
-  const [currentStep, setCurrentStep] = useState(12)
+  const [currentStep, setCurrentStep] = useState(-1)
   const [quizAnswers, setQuizAnswers] = useState(new Array(2))
   const { t } = useTranslation()
 
