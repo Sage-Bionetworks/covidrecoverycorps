bcb17e2552b4a7cc1d55f3021e000fdfb72fea7e
Alina Gendel <alina.gendel@sagebase.org>
2020-06-30 14:14:40 -0700

    add map and location to the appointment
    https://github.com/Sage-Bionetworks/covidrecoverycorps/issues/551

--- src/components/static/Appointment.tsx
+++ src/components/static/Appointment.tsx
@@ -1,26 +1,69 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles, Grid, Card } from '@material-ui/core'
-import { ReportData, AppointmentParticipant } from '../../types/types'
+import {
+  AppointmentParticipant,
+  ColumbiaAppointmentAddress,
+  GoogleAPIKey,
+} from '../../types/types'
 import { ReactComponent as ColumbiaLogo } from '../../assets/columbia_logo.svg'
 import { ReactComponent as SageLogo } from '../../assets/sage_logo.svg'
+import { ReactComponent as MapMarker } from '../../assets/map_marker.svg'
 import QRCode from 'qrcode.react'
-import moment from 'moment'
+import moment, { Moment } from 'moment'
 import { UserService } from '../../services/user.service'
 import i18next from 'i18next'
 import { useTranslation } from 'react-i18next'
 import 'moment/locale/es'
+import GoogleMapReact from 'google-map-react'
+import Geocode from 'react-geocode'
+
+const MarkerComponent = ({ text }: any) => (
+  <div>
+    <MapMarker />
+    <div style={{ width: '100px', fontSize: '1.2rem', textAlign: 'center' }}>
+      {text}
+    </div>
+  </div>
+)
 
 type AppointmentProps = {
   token?: string
 }
+
+type LocationObject = {
+  address?: string
+  addressObject: ColumbiaAppointmentAddress
+  lat?: number
+  lng?: number
+}
+
+type BookedAppointment = {
+  start: Moment
+  patient: {
+    reference: string
+    display: string
+  }
+  address?: LocationObject
+}
+
+Geocode.setApiKey(GoogleAPIKey)
+Geocode.enableDebug()
+
+// set response language. Defaults to english.
+Geocode.setLanguage(i18next.language)
 
 export const useStyles = makeStyles(theme => ({
   root: {
     backgroundColor: '#f5f5f5',
   },
   appointmentContainerDiv: {
-    margin: '0px 30px 70px 30px',
+    margin: '0px 30px 0 30px',
   },
+  mapContainerDiv: {
+    height: '400px',
+    margin: '0 -30px 0 -30px',
+  },
+
   appointmentDateHeader: {
     color: '#FC9090',
     fontWeight: 'bold',
@@ -60,10 +103,38 @@ export const Appointment: React.FunctionComponent<AppointmentProps> = ({
   token,
 }: AppointmentProps) => {
   const classes = useStyles()
-  const [appointment, setAppointment] = useState<ReportData>()
+  const [appointment, setAppointment] = useState<BookedAppointment>()
+
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState()
   const { t } = useTranslation()
+
+  const getCodedLocation = async (
+    addressObject: ColumbiaAppointmentAddress,
+  ): Promise<LocationObject> => {
+    const result: LocationObject = {
+      addressObject,
+    }
+
+    try {
+      const address = `${addressObject.line.join(',')}, ${
+        addressObject.city
+      }, ${addressObject.state}  ${addressObject.postalCode}`
+      result.address = address
+    } catch {
+      return Promise.resolve(result)
+    }
+    try {
+      const response = await Geocode.fromAddress(result.address)
+      const { lat, lng } = response.results[0].geometry.location
+      result.lat = lat as number
+      result.lng = lng as number
+    } catch (e) {
+      setError(e)
+    } finally {
+      return result
+    }
+  }
 
   useEffect(() => {
     let isSubscribed = true
@@ -75,7 +146,18 @@ export const Appointment: React.FunctionComponent<AppointmentProps> = ({
           if (appointmentsResponse?.data?.items?.length > 0) {
             const appt = appointmentsResponse.data.items[0]
             if (appt.data.status === 'booked') {
-              setAppointment(appt)
+              const patient = getInfoPiece(appt.data.participant, 'Patient')!
+              const location = getInfoPiece(appt.data.participant, 'Location')
+              const codedLocation = location?.address
+                ? await getCodedLocation(location?.address)
+                : undefined
+              const appointment: BookedAppointment = {
+                start: moment(appt.data.start),
+                patient,
+                address: codedLocation,
+              }
+
+              setAppointment(appointment)
             }
           }
         } catch (e) {
@@ -100,19 +182,15 @@ export const Appointment: React.FunctionComponent<AppointmentProps> = ({
     )
     return result?.actor
   }
-  const renderAppointment = (appointment: ReportData) => {
-    const appointmentDateTime = moment(appointment.data.start)
-    const patient = getInfoPiece(appointment.data.participant, 'Patient')
-    const location = getInfoPiece(appointment.data.participant, 'Location')
 
-    const reference = patient?.reference.split('Patient/')[1]
+  const renderAppointment = (appointment: BookedAppointment) => {
+    const appointmentDateTime = appointment.start
+
+    const reference = appointment.patient.reference.split('Patient/')[1]
     const friendlyAppointmentTimeStart = appointmentDateTime
       .locale(i18next.language)
       .format('h:mm a')
-    const appointmentDateTimeEnd = moment(appointment.data.start).add(
-      30,
-      'minutes',
-    )
+    const appointmentDateTimeEnd = appointment.start.add(30, 'minutes')
     const friendlyAppointmentTimeEnd = appointmentDateTimeEnd
       .locale(i18next.language)
       .format('h:mm a')
@@ -121,7 +199,6 @@ export const Appointment: React.FunctionComponent<AppointmentProps> = ({
       <Card className={classes.root}>
         <div className={classes.appointmentContainerDiv}>
           <h2 className="text-center">Appointment confirmation</h2>
-
           <p>Your lab appointment has been confirmed for:</p>
           <Grid container direction="row" justify="center" alignItems="center">
             <Grid item>
@@ -144,23 +221,36 @@ export const Appointment: React.FunctionComponent<AppointmentProps> = ({
               </div>
             </Grid>
           </Grid>
-          <Grid container direction="row" justify="center" alignItems="center">
-            <Grid item>
-              <div className={classes.appointmentDateHeader}>LOCATION</div>
+          {appointment.address?.address && (
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="center"
+            >
+              <Grid item>
+                <div className={classes.appointmentDateHeader}>LOCATION</div>
+                {appointment.address.addressObject.line[0]
+                  ? appointment.address.addressObject.line[0]
+                  : ''}
 
-              <div>
-                [TBD]
-              </div>
-              <div>
-                <strong>
-                 
-                 [TBD]
-                </strong>
-              </div>
-              
+                {appointment.address.addressObject.line[1] && (
+                  <div>appointment.address.addressObject.line[1]</div>
+                )}
+                <div>
+                  {`${appointment.address.addressObject.city}, ${appointment.address.addressObject.state} ${appointment.address.addressObject.postalCode}`}
+                </div>
+                <a
+                  href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+                    appointment.address.address,
+                  )}`}
+                  target="_blank"
+                >
+                  View Map
+                </a>
+              </Grid>
             </Grid>
-          </Grid>
-
+          )}
           <div className={classes.appointmentInstructions}>
             <p>
               You will stop at the main Information desk in the lobby and will
@@ -190,11 +280,30 @@ export const Appointment: React.FunctionComponent<AppointmentProps> = ({
               </a>
             </p>
           </div>
+
           <div className={classes.logosDiv}>
             <ColumbiaLogo className={classes.logo} />
             <div className={classes.logosDivSeparator}></div>
             <SageLogo className={classes.logo} />
           </div>
+          {appointment.address?.lat !== undefined && (
+            <div className={classes.mapContainerDiv}>
+              <GoogleMapReact
+                bootstrapURLKeys={{ key: GoogleAPIKey }}
+                defaultCenter={{
+                  lat: appointment.address!.lat!,
+                  lng: appointment.address.lng!,
+                }}
+                defaultZoom={11}
+              >
+                <MarkerComponent
+                  lat={appointment.address!.lat!}
+                  lng={appointment.address!.lng}
+                  text={appointment.address.addressObject.line[0]}
+                />
+              </GoogleMapReact>
+            </div>
+          )}
         </div>
       </Card>
     )
