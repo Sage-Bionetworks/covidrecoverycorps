fece1ed36aa9d77b95fe885c7c5c512c62313cdf
Alina Gendel <alina.gendel@sagebase.org>
2020-12-08 11:06:12 -0800

    faq issue

--- src/components/learningHub/LearningHub5.tsx
+++ src/components/learningHub/LearningHub5.tsx
@@ -22,27 +22,11 @@ const LearningHub5: FunctionComponent = () => {
   console.log('lh53')
   const classes = useStyles()
   const { t } = useTranslation()
-  const [arr, setArr] = React.useState(new Array(5))
-  console.log(arr)
+  const [arr] = React.useState(new Array(5))
   return (
     <div>
       <h3>{t('learningHub.lh5.title')}</h3>
-      <ExpansionPanel key={`faq_0`}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography className={classes.heading}>
-              {t(`learningHub.lh5.q1`)}
-            </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography>{t(`learningHub.lh5.a1`)}</Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
       {[...arr].map((item, index) => (
-       
         <ExpansionPanel key={`faq_${index}`}>
           <ExpansionPanelSummary
             expandIcon={<ExpandMoreIcon />}
