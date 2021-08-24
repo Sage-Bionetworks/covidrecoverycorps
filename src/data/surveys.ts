8a0d0b7ea79ecaecaa3c2e51bb1eea6152303e37
Alina Gendel <alina.gendel@sagebase.org>
2021-08-24 12:28:29 -0700

    update monthly survey to short survey

--- src/data/surveys.ts
+++ src/data/surveys.ts
@@ -64,13 +64,13 @@ export const SURVEYS: SurveyConfigObject = {
   },*/
 
   POST_LAB_MONTHLY: {
-    formSchema: async () => await import(`./postLabMonthly_formSchema${postfix}.json`),
-    uiSchema: async () => await import(`./postLabMonthly_uiSchema${postfix}.json`),
-    navSchema: async () => await import(`./postLabMonthly_navSchema${postfix}.json`),
+    formSchema: async () => await import(`./postLabShort_formSchema${postfix}.json`),
+    uiSchema: async () => await import(`./postLabShort_uiSchema${postfix}.json`),
+    navSchema: async () => await import(`./postLabShort_navSchema${postfix}.json`),
   },
   RESULT_UPLOAD_MONTHLY: {
-    formSchema: async () => await import(`./postLabMonthly_formSchema${postfix}.json`),
-    uiSchema: async () => await import(`./postLabMonthly_uiSchema${postfix}.json`),
-    navSchema: async () => await import(`./postLabMonthly_navSchema${postfix}.json`),
+    formSchema: async () => await import(`./postLabShort_formSchema${postfix}.json`),
+    uiSchema: async () => await import(`./postLabShort_uiSchema${postfix}.json`),
+    navSchema: async () => await import(`./postLabShort_navSchema${postfix}.json`),
   },
 }
